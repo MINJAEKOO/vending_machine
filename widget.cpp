3316ae6e71cc781686f5528274648af2d167a4e5
MINJAEKOO <alswo9085@naver.com>
2019-07-12 16:50:51 +0900

    Make vending_machine

---
+++ widget.cpp
@@ -0,0 +1,58 @@
+#include "widget.h"
+#include "ui_widget.h"
+#include <QMessageBox>
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+void Widget::changeMoney(int n)
+{
+    money += n;
+    ui->lcdNumber->display(QString::number(money));
+    //ui->pbCoffee->setEnabled(false);
+    QMessageBox msg;
+    msg.information(nullptr, "거스름돈", "가져가");
+}
+
+void Widget::on_pb10_clicked()
+{
+    changeMoney(10);
+}
+
+void Widget::on_pb50_clicked()
+{
+    changeMoney(50);
+}
+
+void Widget::on_pb100_clicked()
+{
+    changeMoney(100);
+}
+
+void Widget::on_pb500_clicked()
+{
+    changeMoney(500);
+}
+
+void Widget::on_pbCoffee_clicked()
+{
+    changeMoney(-100);
+}
+
+void Widget::on_pbTea_clicked()
+{
+    changeMoney(-150);
+}
+
+void Widget::on_pbCass_clicked()
+{
+    changeMoney(-200);
+}
